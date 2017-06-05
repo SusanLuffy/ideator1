4c4cf7b4c75d2f3a64f67d06f469ffadfebf9f49
Susan <susanfuhn@gmail.com>
2017-06-05 10:50:04 +0000

    Vaidate User Input

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -5,10 +5,28 @@ def index
 end
 def create
 @idea = Idea.create(idea_params)
+if @idea.valid?
+    
+else
+    
+end
 redirect_to root_path
+end
+def edit
 end
 private
 def idea_params
 params.require(:idea).permit(:description, :author)
 end
+end
+def edit 
+    @idea = Idea.find(params[:id])
+end
+def update
+   @idea = Idea.find(params[:id])
+   if @idea.update(ideas_params)
+       redirect_to root_path
+   else
+       redirect_to edit_idea_path(params[:id])
+   end
 end
